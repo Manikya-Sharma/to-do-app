ca63e4a7e60fa16675c2b5388da2d4b37b26277b
Manikya-Sharma <https://github.com/Manikya-Sharma>
2023-05-03 18:19:23 +0530

    added toggle between sheets

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,18 +1,53 @@
-export default function Lists() {
-  return (
-    <div className="w-[30%] mx-auto">
-      <div>
-        <h2 className="text-center bg-emerald-400 font-semibold text-2xl text-white drop-shadow-md shadow-md py-5">
-          Lists
-        </h2>
+import { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Lists extends Component {
+  state = {
+    tasks: [],
+  };
+
+  changeSheet(elem) {
+    this.props.sheetChanger(this.state.tasks.indexOf(elem));
+  }
+
+  async componentDidMount() {
+    const response = await fetch("../src/data/userData.json");
+    const data = await response.json();
+    this.setState({ tasks: data.tasks });
+  }
+
+  render() {
+    return (
+      <div className="w-[30%] mx-auto">
+        <div>
+          <h2 className="text-center bg-emerald-400 font-semibold text-2xl text-white drop-shadow-md shadow-md py-5">
+            Lists
+          </h2>
+        </div>
+        <div className="bg-teal-100">
+          <ul className="text-lg font-medium flex flex-col gap-5 leading-relaxed py-5 text-cyan-800">
+            {this.state.tasks
+              ? this.state.tasks.map((elem) => {
+                  return (
+                    <li
+                      key={elem}
+                      className="ml-2 cursor-pointer w-fit"
+                      onClick={() => {
+                        this.changeSheet(elem);
+                      }}
+                    >
+                      {elem[0]}
+                    </li>
+                  );
+                })
+              : ""}
+          </ul>
+        </div>
       </div>
-      <div className="bg-teal-100">
-        <ul className="text-lg font-medium flex flex-col gap-5 leading-relaxed py-5 text-cyan-800">
-            <li className="ml-2">Sample List 1</li>
-            <li className="ml-2">Sample List 2</li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
+Lists.propTypes = {
+  sheetChanger: PropTypes.func.isRequired,
+};
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,15 +3,14 @@ import Task from "./Task";
 import PropTypes from "prop-types";
 
 export default class Tasks extends Component {
-
   state = {
-    tasks:[]
-  }
+    tasks: [],
+  };
 
   async componentDidMount() {
     const response = await fetch("../src/data/userData.json");
     const data = await response.json();
-    this.setState({tasks: data.tasks});
+    this.setState({ tasks: data.tasks });
   }
 
   render() {
@@ -24,14 +23,15 @@ export default class Tasks extends Component {
         </div>
         <div className="bg-teal-200 pb-auto">
           <ul className="text-lg font-medium flex flex-col gap-5 leading-relaxed py-5 text-cyan-900">
-            {this.state.tasks[this.props.currentSheet] ? this.state.tasks[this.props.currentSheet].map((elem)=>{
-              return (
-                <li key={elem[0]} className="ml-2">
-                  <Task title={elem[0]} desc={elem[1]} />
-                </li>
-              )
-            }) : ""}
-
+            {this.state.tasks[this.props.currentSheet]
+              ? this.state.tasks[this.props.currentSheet][1].map((elem) => {
+                  return (
+                    <li key={elem[0]} className="ml-2">
+                      <Task title={elem[0]} desc={elem[1]} />
+                    </li>
+                  );
+                })
+              : ""}
           </ul>
         </div>
       </div>
