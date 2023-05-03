23713b0d0722c18323287c6f7cd2b82c74bc30f7
Manikya-Sharma <https://github.com/Manikya-Sharma>
2023-05-03 15:35:14 +0530

    read from json file and added sheet tasks to app

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -1,12 +1,15 @@
-import Lists from "./Lists"
-import Tasks from "./Tasks"
+import { useState } from "react";
+import Lists from "./Lists";
+import Tasks from "./Tasks";
 
-export default function MainApp(){
-    return (
-        <div className="flex gap-0 pt-5">
-            <Lists/>
-            <div className="w-[2px] bg-zinc-500"></div>
-            <Tasks/>
-        </div>
-    )
+export default function MainApp() {
+  const [currentSheet, setCurrentSheet] = useState(0);
+
+  return (
+    <div className="flex gap-0 pt-5">
+      <Lists sheetChanger={setCurrentSheet} />
+      <div className="w-[2px] bg-zinc-500"></div>
+      <Tasks currentSheet={currentSheet} />
+    </div>
+  );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,22 +1,44 @@
+import { Component } from "react";
 import Task from "./Task";
+import PropTypes from "prop-types";
 
-export default function Tasks() {
-  return (
-    <div className="flex-grow">
-      <div>
-        <h2 className="text-center bg-emerald-600 font-semibold text-2xl text-slate-200 drop-shadow-md shadow-md py-5">
-          Tasks
-        </h2>
+export default class Tasks extends Component {
+
+  state = {
+    tasks:[]
+  }
+
+  async componentDidMount() {
+    const response = await fetch("../src/data/userData.json");
+    const data = await response.json();
+    this.setState({tasks: data.tasks});
+  }
+
+  render() {
+    return (
+      <div className="flex-grow">
+        <div>
+          <h2 className="text-center bg-emerald-600 font-semibold text-2xl text-slate-200 drop-shadow-md shadow-md py-5">
+            Tasks
+          </h2>
+        </div>
+        <div className="bg-teal-200 pb-auto">
+          <ul className="text-lg font-medium flex flex-col gap-5 leading-relaxed py-5 text-cyan-900">
+            {this.state.tasks[this.props.currentSheet] ? this.state.tasks[this.props.currentSheet].map((elem)=>{
+              return (
+                <li key={elem[0]} className="ml-2">
+                  <Task title={elem[0]} desc={elem[1]} />
+                </li>
+              )
+            }) : ""}
+
+          </ul>
+        </div>
       </div>
-      <div className="bg-teal-200 pb-auto">
-      <ul className="text-lg font-medium flex flex-col gap-5 leading-relaxed py-5 text-cyan-900">
-            <li className="ml-2">Sample Task 1</li>
-            <li className="ml-2">Sample Task 2</li>
-            <li className="ml-2">
-                <Task title="Sample Task 3" desc="I am a sample task"/>
-            </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+Tasks.propTypes = {
+  currentSheet: PropTypes.number,
+};
