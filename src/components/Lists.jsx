de97e9fc8ea042ff7c1ed0d64040a99c2a2bb04c
Manikya-Sharma <https://github.com/Manikya-Sharma>
2023-05-04 19:16:36 +0530

    made changes to ui/ux

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 
 export default class Lists extends Component {
   state = {
+    username: "",
+    password: "",
     tasks: [],
   };
 
@@ -16,11 +18,17 @@ export default class Lists extends Component {
     this.setState({ tasks: data.tasks });
   }
 
+  async componentDidUpdate() {
+    const response = await fetch("http://localhost:8000");
+    const data = await response.json();
+    this.setState(data);
+  }
+
   render() {
     return (
       <div className="flex overflow-x-auto overflow-y-hidden border-t-2 bg-teal-100 sm:mx-auto sm:block sm:w-[30%] sm:border-none">
         <div>
-          <h2 className="lists-text h-56 -rotate-180 bg-emerald-400 text-center text-2xl font-semibold text-white shadow-md drop-shadow-md sm:h-fit sm:rotate-0 sm:py-5 ">
+          <h2 className="lists-text h-56 -rotate-180 bg-emerald-400 text-center text-2xl font-semibold text-white shadow-md drop-shadow-md sm:h-fit sm:rotate-0 sm:py-5 sm:text-5xl ">
             Lists
           </h2>
         </div>
@@ -41,6 +49,12 @@ export default class Lists extends Component {
                   );
                 })
               : ""}
+            <li
+              className="ml-2 min-w-fit max-w-full cursor-pointer rounded-xl bg-teal-200 px-6 py-4 transition-all duration-300 hover:bg-teal-300 border-[5px] border-dotted border-teal-400 hover:border-teal-800 hover:text-xl"
+              onClick={{}}
+            >
+              + New Sheet
+            </li>
           </ul>
         </div>
       </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -15,6 +15,12 @@ export default class Tasks extends Component {
     this.setState(data);
   }
 
+  async componentDidUpdate() {
+    const response = await fetch("http://localhost:8000");
+    const data = await response.json();
+    this.setState(data);
+  }
+
   findTask = (tasks, sheet, title) => {
     let index2 = 0;
     for (const taskNum of tasks[sheet][1]) {
@@ -45,10 +51,15 @@ export default class Tasks extends Component {
   render() {
     return (
       <div className="flex-grow overflow-hidden">
-        <div>
-          <h2 className="bg-emerald-600 py-5 text-center text-2xl font-semibold text-slate-200 shadow-md drop-shadow-md">
-            Tasks
-          </h2>
+        <div className="flex justify-around bg-emerald-600 py-5 shadow-md drop-shadow-md text-2xl font-semibold text-slate-200">
+          <div>
+            <h2 className="text-5xl text-center">
+              Tasks {this.state.tasks[this.props.currentSheet]?<> - {this.state.tasks[this.props.currentSheet][0]}</>: ""}
+            </h2>
+          </div>
+          <button className="py-2 px-5 bg-green-300 text-black/80 hover:bg-green-100 hover:text-black rounded-xl transition-all duration-300 border-2 border-green-300 hover:border-green-500 shadow-lg hover:text-[1.55rem]">
+            + New Task
+          </button>
         </div>
         <div className="h-full bg-teal-200">
           <ul className="flex flex-col gap-5 py-5 text-lg font-medium leading-relaxed text-cyan-900">
